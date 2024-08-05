3fb17c14cb1fb00810123b73387e6ee829689bce
saumitraebits <saumitra@ealphabits.com>
2024-08-05 21:59:16 +0530

    expenditure-analysis-complete

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,70 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  if (!transactions.length) return [];
+
+  let newMap = new Map();
+  newMap.set(transactions[0].category, {
+    category: transactions[0].category,
+    totalSpent: transactions[0].price,
+  });
+
+  for (let i = 1; i <= transactions.length - 1; i++) {
+    const existingCategory = newMap.get(transactions[i].category);
+    if (existingCategory) {
+      newMap.set(existingCategory.category, {
+        category: existingCategory.category,
+        totalSpent: existingCategory.totalSpent + transactions[i].price,
+      });
+    } else {
+      newMap.set(transactions[i].category, {
+        category: transactions[i].category,
+        totalSpent: transactions[i].price,
+      });
+    }
+  }
+
+  const output = Array.from(newMap.values());
+  console.log(output);
+  return output;
 }
+
+calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: "Food",
+    itemName: "Burger",
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: "Clothing",
+    itemName: "T-Shirt",
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: "Electronics",
+    itemName: "Headphones",
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: "Clothing",
+    itemName: "Jeans",
+  },
+]);
 
 module.exports = calculateTotalSpentByCategory;
